d68ae00ee0513fe13bfe30e296190e8161d5207f
ronald sarmiento <ronisarmont@gmail.com>
2023-07-16 12:08:51 -0400

    DELETE request implemented

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -32,4 +32,15 @@ public class StudentService {
         studentRepository.save(student);
         //System.out.println(student);
     }
+
+    public void deleteStudent(Long studentId) {
+
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("student with id "+
+                    studentId + " does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
 }
