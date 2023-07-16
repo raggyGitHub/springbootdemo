703649fd6d385d361a4a9ec1b00538bb182a031d
ronald sarmiento <ronisarmont@gmail.com>
2023-07-16 11:48:40 -0400

    POST request testing

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -24,6 +25,11 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional=studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("Email taken !!");
+        }
+        studentRepository.save(student);
+        //System.out.println(student);
     }
 }
